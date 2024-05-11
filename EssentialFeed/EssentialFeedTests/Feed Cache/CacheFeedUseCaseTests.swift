be5f1fe78badc44f88cd2d42d24972459bb0310c
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-12 00:40:09 +0800

    Load command delivers cached images on less than seven days old cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -131,7 +131,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
     
     private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
         let models = [uniqueImage(), uniqueImage()]
-        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.url) }
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
         return (models, local)
     }
     
