52f2420c9b036d77f2b73401737a0d54ea7d90a4
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-14 17:33:25 +0800

    Make LocalFeedLoader conform to FeedLoader

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public final class LocalFeedLoader {
+    
     private let store: FeedStore
     private let currentDate: () -> Date
     private let calendar = Calendar(identifier: .gregorian)
@@ -56,7 +57,7 @@ extension LocalFeedLoader {
     }
 }
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
     
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
