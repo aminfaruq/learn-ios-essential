f277424a757c9206ebe4c9c3e98bd1f007e1af5d
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-14 17:14:05 +0800

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Phincon on 14/05/24.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
