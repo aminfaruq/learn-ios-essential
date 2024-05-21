cec3a810b03e2a22c4d19e6661d1245913e6bd7d
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-21 14:08:54 +0800

    Add empty CoreData feed store specs

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+final class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     
     override func setUp() {
         super.setUp()
