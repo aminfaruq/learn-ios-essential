e1c31491999d269d0467b24a0072e72f2472fb68
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-21 16:01:31 +0800

    remove local variable

--- EssentialFeed/EssentialFeed/Feed Cache/Insfrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Insfrastructure/CoreData/CoreDataFeedStore.swift
@@ -17,7 +17,6 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        let context = self.context
         perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
@@ -32,7 +31,6 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
         perform { context in
             do {
                 let manageCache = try ManagedCache.newUniqueInstance(in: context)
@@ -47,7 +45,6 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
         perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
