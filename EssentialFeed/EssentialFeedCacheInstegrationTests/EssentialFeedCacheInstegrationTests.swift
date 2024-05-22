f865267db0da0664450046e92b6690613e78b7f7
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-22 22:39:45 +0800

    Extract duplicate cache load expectations into a shared helper method

--- EssentialFeed/EssentialFeedCacheInstegrationTests/EssentialFeedCacheInstegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheInstegrationTests/EssentialFeedCacheInstegrationTests.swift
@@ -25,19 +25,7 @@ final class EssentialFeedCacheInstegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, [], "Expected empty feed")
-                
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toLoad: [])
     }
     
     func test_load_deliversItemsSavedOnASeparateInstance() {
@@ -52,19 +40,23 @@ final class EssentialFeedCacheInstegrationTests: XCTestCase {
         }
         wait(for: [saveExp], timeout: 1.0)
         
-        let loadExp = expectation(description: "Wait for load completion")
-        sutToPerformLoad.load { loadResult in
-            switch loadResult {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, feed)
+        expect(sutToPerformLoad, toLoad: feed)
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(loadedFeed):
+                XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
                 
             case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
+                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
             }
             
-            loadExp.fulfill()
+            exp.fulfill()
         }
-        wait(for: [loadExp], timeout: 1.0)
+        wait(for: [exp], timeout: 1.0)
     }
     
     // - MARK: Helpers
