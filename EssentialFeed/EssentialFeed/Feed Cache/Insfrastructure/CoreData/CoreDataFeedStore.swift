2cfd80d4a0c338608f80e4a03f3a6d20a7555d54
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-23 14:27:45 +0800

    Simplify `CoreDataFeedStore` completion code with the new `Result ` APIs

--- EssentialFeed/EssentialFeed/Feed Cache/Insfrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Insfrastructure/CoreData/CoreDataFeedStore.swift
@@ -18,40 +18,30 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                if let cache = try ManagedCache.find(in: context) {
-                    completion(.success(.some((feed: cache.localFeed, timestamp: cache.timestamp))))
-                } else {
-                    completion(.success(.none))
+            completion(Result {
+                try ManagedCache.find(in: context).map { cache in
+                    CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)
                 }
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion(Result {
                 let manageCache = try ManagedCache.newUniqueInstance(in: context)
                 manageCache.timestamp = timestamp
                 manageCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 try context.save()
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            do {
+            completion(Result {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
