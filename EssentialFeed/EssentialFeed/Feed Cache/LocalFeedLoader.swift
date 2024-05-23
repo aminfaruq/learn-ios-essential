846fefd20cb2805c6715737b7f1815946e840839
Amin Faruq <aminfaruk.fa@gmail.com>
2024-05-23 14:20:46 +0800

    Replace occurrences of `Error?` for representing operation success/failure with `Result<Void, Error>`

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -18,25 +18,27 @@ public final class LocalFeedLoader {
 }
 
 extension LocalFeedLoader {
-    public typealias SaveResult = Error?
+    public typealias SaveResult = Result<Void, Error>
     
-    public func save(_ items: [FeedImage], completion: @escaping (SaveResult) -> Void) {
-        self.store.deleteCachedFeed() { [weak self] error in
+    public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
+        self.store.deleteCachedFeed() { [weak self] deletionResult in
             guard let self = self else { return }
             
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                cache(items, with: completion)
+            switch deletionResult {
+            case .success:
+                self.cache(feed, with: completion)
+                
+            case let .failure(error):
+                completion(.failure(error))
             }
         }
     }
     
     private func cache(_ items: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
-        self.store.insert(items.toLocal(), timestamp: self.currentDate()) {[weak self] error in
+        self.store.insert(items.toLocal(), timestamp: self.currentDate()) {[weak self] insertionResult in
             guard self != nil else { return }
             
-            completion(error)
+            completion(insertionResult)
         }
     }
 }
--- EssentialFeed/EssentialFeedCacheInstegrationTests/EssentialFeedCacheInstegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheInstegrationTests/EssentialFeedCacheInstegrationTests.swift
@@ -33,12 +33,7 @@ final class EssentialFeedCacheInstegrationTests: XCTestCase {
         let sutToPerformLoad = makeSUT()
         let feed = uniqueImageFeed().models
         
-        let saveExp = expectation(description: "Wait for save completion")
-        sutToPerformSave.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully")
-            saveExp.fulfill()
-        }
-        wait(for: [saveExp], timeout: 1.0)
+        save(feed, with: sutToPerformSave)
         
         expect(sutToPerformLoad, toLoad: feed)
     }
@@ -58,8 +53,10 @@ final class EssentialFeedCacheInstegrationTests: XCTestCase {
     
     private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #filePath, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
+        loader.save(feed) { result in
+            if case let Result.failure(error) = result {
+                XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
+            }
             saveExp.fulfill()
         }
         wait(for: [saveExp], timeout: 1.0)
