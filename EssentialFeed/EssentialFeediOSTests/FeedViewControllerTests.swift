49fc0e12c5b50d96b87d63a92a9807d0de4e07b4
Amin Faruq <aminfaruk.fa@gmail.com>
2024-08-15 00:28:14 +0800

    Extract UIImage test helper to a separate file

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -477,15 +477,3 @@ private extension UIButton {
         }
     }
 }
-
-private extension UIImage {
-    static func make(withColor color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        defer { UIGraphicsEndImageContext() }
-        
-        color.setFill()
-        UIRectFill(CGRect(origin: .zero, size: size))
-        
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-}
--- EssentialFeed/EssentialFeediOSTests/Helpers/UIImage+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -5,4 +5,16 @@
 //  Created by Amin faruq on 10/08/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        
+        color.setFill()
+        UIRectFill(CGRect(origin: .zero, size: size))
+        
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
