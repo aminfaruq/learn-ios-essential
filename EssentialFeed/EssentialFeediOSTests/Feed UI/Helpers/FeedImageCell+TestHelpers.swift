fd405539774c63846b944064cf2ca34173c2386e
Amin Faruq <aminfaruk.fa@gmail.com>
2024-08-23 00:22:17 +0800

    Foldering Test Case

---
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,26 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Phincon on 23/08/24.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+extension FeedImageCell {
+    func simulateRetryAction() { feedImageRetryButton.simulateTap()}
+
+    var isShowingLocation: Bool { !locationContainer.isHidden }
+    
+    var isShowingImageLoadingIndicator: Bool { feedImageContainer.isShimmering }
+    
+    var locationText: String? { locationLabel.text }
+    
+    var descriptionText: String? { descriptionLabel.text }
+    
+    var isShowingRetryAction: Bool { !feedImageRetryButton.isHidden }
+    
+    var renderedImage: Data? { feedImageView.image?.pngData() }
+}
