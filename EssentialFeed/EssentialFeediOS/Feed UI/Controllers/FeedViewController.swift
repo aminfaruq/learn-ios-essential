7a1b2c6119cb37ad312ad8c52699d52973bb99a3
Amin Faruq <aminfaruk.fa@gmail.com>
2024-08-23 00:08:02 +0800

    implement `FeedViewController`

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -20,7 +20,9 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.register(FeedImageCell.self, forCellReuseIdentifier: "FeedImageCell")
+        tableView.dataSource = self
+        tableView.delegate = self
         tableView.prefetchDataSource = self
         refreshControl = refreshController?.view
         refreshController?.refresh()
@@ -31,7 +33,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return cellController(forRowAt: indexPath).view()
+        return cellController(forRowAt: indexPath).view(in: tableView)
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
