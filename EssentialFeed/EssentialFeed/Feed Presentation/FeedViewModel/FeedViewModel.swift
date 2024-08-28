21a9f9cbc36848dbbf518bec0875925c8deb19dc
Amin Faruq <aminfaruk.fa@gmail.com>
2024-08-28 14:12:20 +0800

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Phincon on 28/08/24.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
